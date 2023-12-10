65886cecf36cdd921c16b4b1cd7b01c91f9559da
Tarsis <br.tmvdl@gmail.com>
2023-12-10 10:07:13 -0300

    Updating app files

--- src/app/main.go
+++ src/app/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+import "flag"
+
 func main() {
-	fmt.Println("Antify")
+	data := *flag.String("data-path", "/data", "Path to save all data.")
+
+	port := *flag.String("port", "555", "Port expose HTTP project files.")
+
+	fmt.Printf("Antify; Data: %s; Port: %s; \n", data, port)
 }
