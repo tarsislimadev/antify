aa520937ad5a72576d2973da2412ed8da2ef24dc
Tarsis <br.tmvdl@gmail.com>
2024-05-22 18:31:09 -0300

    Updating app files

--- src/dotnet/Program.cs
+++ src/dotnet/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace MyApp
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
