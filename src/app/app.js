15b8fb2baef51f1a17fb6ba9045603225e0a21d2
Tarsis <br.tmvdl@gmail.com>
2024-05-23 16:44:11 -0300

    Updating app files

--- src/app/app.js
+++
@@ -1,7 +0,0 @@
-import { Router } from './libs/router/index.js'
-
-const app = new Router()
-
-app.get('/', (_, res) => res.setJSON({ date: Date.now() }))
-
-export default app
--- src/app/libs/http/response.messages.js
+++
@@ -1,9 +0,0 @@
-// 
-
-export default {
-  '200': 'OK',
-  '400': 'CLIENT ERROR',
-  '403': 'FORBIDDEN',
-  '404': 'NOT FOUND',
-  '500': 'SERVER ERROR',
-}
