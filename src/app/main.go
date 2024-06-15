d2618d01f3fe2add7eaf35eed9cdd10555009bff
Tarsis <br.tmvdl@gmail.com>
2024-06-15 05:44:02 -0300

    Updating app files

--- src/app/main.go
+++ src/app/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
-func main () {
-	fmt.Println("hello world");
+func login(username string, password string) string {
+  return "username: " + username + "; " + "password: " + password + "; \n"
+}
+
+func http_login(w http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+  username := query["username"][0]
+	password := query["password"][0]
+
+  fmt.Fprintf(w, login(username, password))
+}
+
+func http_createuser(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(w, "createuser\n")
+}
+
+func main() {
+  http.HandleFunc("/login", http_login)
+  http.HandleFunc("/createuser", http_createuser)
+
+  http.ListenAndServe(":80", nil)
 }
